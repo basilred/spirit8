b5726e2596e109078553e62b6bf039369900dd0a
Vassily Krasnov <mykrasnov@gmail.com>
2016-03-03 22:27:08 +0300

    Отказаться от радиогруппы в пользу своего простого управления слайдером

--- common.blocks/slider/slider.deps.js
+++ common.blocks/slider/slider.deps.js
@@ -2,6 +2,7 @@
     mustDeps : [
         { block : 'slider-item' },
         { block : 'slider', elem : 'roller' },
-        { block : 'slider', elem : 'controller' }
+        { block : 'slider', elem : 'controller' },
+        { block : 'slider', elem : 'pin' }
     ]
 })
