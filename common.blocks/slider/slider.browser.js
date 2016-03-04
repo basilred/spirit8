b53d7038829ce5ca0095eb9a1f5423d4ef42ed16
Vassily Krasnov <mykrasnov@gmail.com>
2016-03-04 20:06:52 +0300

    Доделать готовый слайдер

--- common.blocks/slider/slider.browser.js
+++ common.blocks/slider/slider.browser.js
@@ -4,19 +4,29 @@ provide(BEMDOM.decl(this.name, {
     onSetMod: {
         js: {
             inited: function() {
-
+                this.sliderOffset = 0;
             }
         }
     },
     onElemSetMod: {
         'pin': {
-            'active': function (elem, modName, prevModVal) {
-
+            'active': {
+                'true': function (elem, modName, prevModVal) {
+                    // Нужно применить transform: translateX(n); к каждому блоку slider-item
+                    var offset = this.sliderOffset;
+                    this.findBlocksInside('slider-item').map(function (item) {
+                        item.domElem.css('transform', 'translateX(-' + offset + '%)');
+                    });
+                }
             }
         }
     },
     onPinClick: function (e) {
-        this.delMod(this.elem('pin'), 'active').setMod(e.currentTarget, 'active');
+        if (!this.hasMod(e.currentTarget, 'active')) {
+            this.sliderOffset = this.elemParams(e.currentTarget).offset;
+            this.delMod(this.elem('pin'), 'active')
+                .setMod(e.currentTarget, 'active');
+        }
     }
 },
 {
