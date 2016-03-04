4839167890b958cc57a7a3d05da885380321e360
Vassily Krasnov <mykrasnov@gmail.com>
2016-03-04 16:48:13 +0300

    Контроллер слайдера начал реагировать на переключение

--- common.blocks/slider/slider.browser.js
+++ common.blocks/slider/slider.browser.js
@@ -7,6 +7,21 @@ provide(BEMDOM.decl(this.name, {
 
             }
         }
+    },
+    onElemSetMod: {
+        'pin': {
+            'active': function (elem, modName, prevModVal) {
+
+            }
+        }
+    },
+    onPinClick: function (e) {
+        this.delMod(this.elem('pin'), 'active').setMod(e.currentTarget, 'active');
+    }
+},
+{
+    live: function () {
+        this.liveBindTo('pin', 'click', this.prototype.onPinClick);
     }
 }));
 
